a98d46ef20cd888d9e58a6156fdc0c8c22f2ba0a
SPELLER228 <151197797+SPELLER228@users.noreply.github.com>
2023-11-27 23:58:22 +0300

    2.3b variant 9

--- Source.cpp
+++ Source.cpp
@@ -1,56 +1,36 @@
 #include <iostream>
-#include <math.h>
-#include <iomanip>
+
 using namespace std;
+
+bool prost(int);
 
 int main()
 {
-	double x, xn, dx, y1=0,y2=0,E=0.000001,y2i;
-	int n,i,N;
-	cin >> x >> xn >> dx;
-	n = (xn - x) / dx;
-	for (x;x < xn;x += dx)
+	int a,b2,b3,b4,b5,b6;
+	
+	for (int i = 100;i < 1000;i++)
 	{
-		x = round(x * 100000) / 100000;
-		if (x == 0)
-		{
-			y1 = 0; y2 = 0; N = 0;
-		}
-		else 
-		{
-			y1 = 1 / (pow((1 + x), 3)) - 1;
-			i = 1;
-			N = 0;
-			y2i = 1;
-			y2 = 0;
-			while ((y2i > E) || (y2i < -E))
-			{
-				y2i = (pow(-1, i) * (i + 1) * (i + 2) * pow(x, i)) / 2;
-				y2 = y2 + y2i;
-				i++;
-				N++;
-			}
-
-		}
-
-			cout
-				<< left
-				<< setw(5)
-				<< x
-				<< ' '
-				<< left
-				<< setw(10)
-				<< y1
-				<< ' '
-				<< left
-				<< setw(10)
-				<< y2
-				<< ' '
-				<< left
-				<< setw(10)
-				<< N
-				<< endl;
+		b2 = i / 100 * 100 + i % 100 / 10 + i % 10 * 10;
+		b3 = i / 100 * 10 + i % 10 * 100 + i % 100 / 10;
+		b4 = i / 100 * 10 + i % 10 + i % 100 / 10 * 100;
+		b5 = i / 100 + i % 10 * 10 + i % 100 / 10*100;
+		b6= i / 100  + i % 10 * 100 + i % 100 / 10*10;
+		if ((prost(i) == 0) &&(prost(b2)==0)&&(prost(b3)==0)&&(prost(b4)==0)&&(prost(b5)==0)&&(prost(b6)==0))
+			cout << i << ' ';
 	}
 		
+	return 0;
+}
+
+bool prost(int a)
+{
+	for (int j = 2;j < sqrt(a);j++)
+	{
+		if (a % j == 0)
+		{
+			return 1;
+			break;
+		}
+	}
 	return 0;
 }
